a20903b241c8318e26f5b4d88ad4ee6594547d65
AClarke6 <alastair_clarke@yahoo.com>
2026-03-27 16:16:21 +0000

    Comment Fixes

---
+++ pg.ps1
@@ -0,0 +1,3 @@
+﻿$env:PGPASSWORD = (dotnet user-secrets list --project DraftReader.Web | Where-Object { $_ -match "PostgresPassword" } | ForEach-Object { $_ -replace "PostgresPassword = ", "" })
+$pgArgs = @("-U", "postgres", "-d", "draftreader") + $args
+& "C:\Program Files\PostgreSQL\18\bin\psql.exe" @pgArgs
