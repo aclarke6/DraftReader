5109ebfb98092860d466f445eefb566068ee37a5
AClarke6 <alastair_clarke@yahoo.com>
2026-04-06 16:23:45 +0100

    Add System Support dashboard (view-only)
    - Introduce SupportController with SystemSupport role restriction
    - Add SupportDashboardViewModel
    - Create initial dashboard view for system operations and support messages

---
+++ DraftView.Web/Controllers/SupportController.cs
@@ -0,0 +1,26 @@
+﻿using DraftView.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DraftView.Web.Controllers
+{
+    [Authorize(Roles = "SystemSupport")]
+    public class SupportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Dashboard");
+        }
+
+        public IActionResult Dashboard()
+        {
+            var model = new SupportDashboardViewModel {
+                SystemStatus = "Operational",
+                ActiveAuthors = 0,
+                ActiveReaders = 0
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ DraftView.Web/Models/SupportViewModels.cs
@@ -0,0 +1,14 @@
+﻿namespace DraftView.Web.Models;
+
+public class SupportDashboardViewModel
+{
+    public string SystemStatus { get; init; } = "Operational";
+    public int ActiveAuthors
+    {
+        get; init;
+    }
+    public int ActiveReaders
+    {
+        get; init;
+    }
+}
